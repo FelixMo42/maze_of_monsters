a70ac0f93b5f7dcdc5936b507a747983db837ab9
Felix Moses <felix@tacocat.com>
2025-04-10 11:21:39 +0200

    wow

--- src/index.tsx
+++ src/index.tsx
@@ -11,18 +11,28 @@ function dealDamage(amu: number): Effect {
 	}
 }
 
+function heal(amu: number): Effect {
+	return {
+		apply: (t) => {
+			t.hp = Math.min(t.hp + amu, t.maxHp)
+		}
+	}
+}
+
 interface Effect {
 	apply: (target: Character) => void
 }
 
 interface Action {
-	name: string,
+	name: string
+	target: "enemy" | "self"
 	effect: Effect
 }
 
 interface Character {
 	name: string
 	hp: number
+	maxHp: number
 	actions: Action[]
 }
 
@@ -38,55 +48,59 @@ type Mode
 const MODE = signal<Mode>({ kind: "normal" });
 const STATE = signal<State>({
 	players: [{
-		name: "Gorbo",
+		name: "Peter",
 		hp: 100,
+		maxHp: 100,
 		actions: [
 			{
-				name: "Attack",
+				name: "KICK ASS",
+				target: "enemy",
 				effect: dealDamage(10)
 			}
 		]
 	}],
 	targets: [{
-		name: "Rat",
-		hp: 20,
+		name: "Guard",
+		hp: 1000,
+		maxHp: 1000,
 		actions: [],
 	}],
 })
 
-function end() {
-	const newState = STATE.value
-
-	newState.players = newState.players.filter((player) => player.hp > 0)
-	newState.targets = newState.targets.filter((player) => player.hp > 0)
-
-	console.log(">>", newState.targets.length)
-
-	STATE.value = { ...newState }
+function ai() {
+	dealDamage(30).apply(STATE.value.players[0])
 }
 
 function act(action: Action) {
 	return () => {
-		MODE.value = {
-			kind: "target",
-			cb: (target) => {
-				// reset target mode
-				MODE.value = { kind: "normal" }
-
-				// apply effect
-				action.effect.apply(target)
-
-				// end of turn things
-				end()
-			}
+		// apply player action
+		if (action.target === "self") {
+			action.effect.apply(STATE.value.players[0])
+		} else {
+			action.effect.apply(STATE.value.targets[0])
 		}
+
+		// enemy action
+		ai()
+
+		// trigger update
+		STATE.value = clone(STATE.value)
 	}
+}
+
+function HPBar(params: { hp: number, maxHP: number }) {
+	const p = `${Math.floor(params.hp / params.maxHP * 100)}%`
+	const fg = `#C08081`
+	const bg = `rgba(0, 0, 0, 0.2)`
+	return <div class="bar" style={{
+		background: `linear-gradient(to right, ${fg} ${p}, ${bg} ${p})`
+	}}>HP: {params.hp}/{params.maxHP}</div>
 }
 
 function Player({player}: {player: Character}) {
 	return <section>
 		<h1>{player.name}</h1>
-		<div>HP: {player.hp}</div>
+		<HPBar hp={player.hp} maxHP={player.maxHp} />
 		{player.actions.map(action =>
 			<input
 				type='button'
@@ -109,26 +123,48 @@ function Target({target}: { target: Character }) {
 
 	return <section style={{ backgroundColor }} onClick={onClick}>
 		<h1>{target.name}</h1>
-		{target.hp}
+		<HPBar hp={target.hp} maxHP={target.maxHp} />
+		<p>"If you want too ascend <u>The Tower of Gilgamesh</u>, you mush show me your <b><i>POWER</i></b>!!!!!!!!"</p>
+		<img src="https://i.imgflip.com/9qcljw.jpg" title="made at imgflip.com"/>
 	</section>
 }
 
 export function App() {
 	const state = STATE.value
 
-	return (
-		<main>
-			<div style={{ display: "flex" }}>
-				<div style={{ flex: 1 }}>
-					{state.players.map(player => <Player player={player} />)}
-				</div>
-
-				<div style={{ flex: 1 }}>
-					{state.targets.map(target => <Target target={target} />)}
-				</div>
-			</div>
+	if (state.players[0].hp <= 0) {
+		return <main>
+			<p>YOU ARE DEAD :(</p>
 		</main>
-	)
+	}
+
+	return <main>
+		<div style={{ display: "flex" }}>
+			<div style={{ flex: 1 }}>
+				{state.players.map(player => <Player player={player} />)}
+			</div>
+
+			<div style={{ flex: 1 }}>
+				{state.targets.map(target => <Target target={target} />)}
+			</div>
+		</div>
+	</main>
 }
 
 render(<App />, document.body)
+
+function clone<T>(t: T): T {
+	if (typeof t === "object") {
+		if (t instanceof Array) {
+			return [...t.map(clone)] as T
+		} else {
+			return Object.fromEntries(
+				Object
+					.entries(t)
+					.map(([k, v]) => [k, clone(v)])
+			) as T
+		}
+	} else {
+		return t
+	}
+}
