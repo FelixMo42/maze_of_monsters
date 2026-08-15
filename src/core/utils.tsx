95a4e8745aeacabbc2a1804ec737c33817b8ea47
Felix Moses <felix@tacocat.com>
2026-08-15 15:20:31 +0400

    tabula rasa

--- src/core/utils.tsx
+++
@@ -1,8 +0,0 @@
-export function selectRandom<T>(arr: T[], filter: (t: T) => boolean): T {
-    const a = arr.filter(filter)
-    return a[Math.floor(Math.random() * a.length)]
-}
-
-export function randomize<T>(arr: T[]): T[] {
-    return arr.sort(() => Math.random() - 0.5)
-}
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
-
-export default defineConfig({
-  base: './', // 👈 Use relative paths
-  plugins: [preact()],
-});
